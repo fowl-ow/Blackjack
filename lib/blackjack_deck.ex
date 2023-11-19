97380db832b57f3d697e2b6cb094a985efeb62f7
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2023-11-19 16:22:05 +0100

    feat[Blackjack.Deck]: Implement GenServer callbacks and client calls

--- lib/blackjack_deck.ex
+++ lib/blackjack_deck.ex
@@ -1,7 +1,32 @@
 defmodule Blackjack.Deck do
+  use GenServer
   @moduledoc """
   Documentation for `Blackjack.Deck`.
   """
+
+
+  # Client
+
+  def start_link(amount) when is_integer(amount) do
+    GenServer.start_link(__MODULE__, amount)
+  end
+
+  def draw(pid, deck) do
+    GenServer.call(pid, {:draw, deck})
+  end
+
+  # Server (callbacks)
+
+  @imp true
+  def init(amount) do
+    {:ok, create_deck(amount)}
+  end
+
+  @imp true
+  def handle_call(:draw, _from, deck) do
+    [ card | rest ] = deck
+    {:reply, card, rest}
+  end
 
   @doc """
   Creates a deck of cards.
