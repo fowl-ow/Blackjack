883aaa8d1662b6f7976e00908c72de44b6cdc818
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2023-11-19 15:52:51 +0100

    refactor: added files for game server

---
+++ lib/blackjack_game.ex
@@ -0,0 +1,12 @@
+defmodule Blackjack.Game do
+  @moduledoc """
+  Documentation for `Game`.
+  """
+
+  @doc """
+  Documentation for `start`.
+
+  ## Examples
+  """
+
+end
---
+++ test/blackjack_game_test.exs
@@ -0,0 +1,3 @@
+defmodule BlackjackGameTest do
+  
+end
